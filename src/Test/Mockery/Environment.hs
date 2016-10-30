8870586990f56aedff4e351ba749ccb021f1515d
Simon Hengel <sol@typeful.net>
2016-10-30 11:57:18 +0800

    Add withModifiedEnvironment

--- src/Test/Mockery/Environment.hs
+++ src/Test/Mockery/Environment.hs
@@ -1,32 +1,53 @@
-module Test.Mockery.Environment (withEnvironment) where
+module Test.Mockery.Environment (
+  withEnvironment
+, withModifiedEnvironment
+) where
 
 import           Control.Exception.Base
 import           Control.Monad
 import           System.Environment.Compat
 
 -- |
--- Run the given action with the specified environment.
+-- Run the given action within the specified environment.
 --
--- Before executing the action, `withEnvironment` backs up the current environment,
--- clears out the environment, and then applies the supplied environment.
+-- Before executing the action, `withEnvironment` backs up the current
+-- environment, clears out the environment, and then applies the supplied
+-- environment.
 -- After the action has completed the original environment is restored.
 --
 -- __Note__: The environment is global to a process, so tests that modify the
 -- environment can no longer be run in parallel.
 withEnvironment :: [(String, String)] -> IO a -> IO a
-withEnvironment environment action = bracket saveEnv restoreEnv $ const action
-  where
-    saveEnv :: IO [(String, String)]
-    saveEnv = do
-      env <- clearEnv
-      forM_ environment $ uncurry setEnv
-      return env
-    restoreEnv :: [(String, String)] -> IO ()
-    restoreEnv env = do
-      _ <- clearEnv
-      forM_ env $ uncurry setEnv
-    clearEnv :: IO [(String, String)]
-    clearEnv = do
-      env <- getEnvironment
-      forM_ env (unsetEnv . fst)
-      return env
+withEnvironment environment action = bracketEnvironment $ do
+  setEnvironment environment
+  action
+
+-- |
+-- Run the given action within an augmented environment.
+--
+-- Before executing the action, `withModifiedEnvironment` backs up the current
+-- environment and then augments it with the supplied values.
+-- After the action has completed the original environment is restored.
+--
+-- __Note__: The environment is global to a process, so tests that modify the
+-- environment can no longer be run in parallel.
+withModifiedEnvironment :: [(String, String)] -> IO a -> IO a
+withModifiedEnvironment environment action = bracketEnvironment $ do
+  modifyEnvironment environment
+  action
+
+bracketEnvironment :: IO a -> IO a
+bracketEnvironment = bracket getEnvironment setEnvironment . const
+
+setEnvironment :: [(String, String)] -> IO ()
+setEnvironment environment = do
+  clearEnvironment
+  modifyEnvironment environment
+
+modifyEnvironment :: [(String, String)] -> IO ()
+modifyEnvironment environment = forM_ environment $ uncurry setEnv
+
+clearEnvironment :: IO ()
+clearEnvironment = do
+  environment <- getEnvironment
+  forM_ environment (unsetEnv . fst)
--- test/Test/Mockery/EnvironmentSpec.hs
+++ test/Test/Mockery/EnvironmentSpec.hs
@@ -8,13 +8,27 @@ import           Test.Mockery.Environment
 spec :: Spec
 spec = do
   describe "withEnvironment" $ do
-    it "runs the given action with the specified environment" $ do
+    it "runs the given action within the specified environment" $ do
       withEnvironment [("foo", "bar")] $ do
         getEnvironment `shouldReturn` [("foo", "bar")]
 
     it "restores the original environment" $ do
       old <- getEnvironment
       withEnvironment [("foo", "bar")] $ do
+        return ()
+      new <- getEnvironment
+      new `shouldMatchList` old
+
+  describe "withModifiedEnvironment" $ do
+    it "runs the given action within a modified environment" $ do
+      original <- getEnvironment
+      withModifiedEnvironment [("foo", "bar")] $ do
+        modified <- getEnvironment
+        modified `shouldMatchList` original ++ [("foo", "bar")]
+
+    it "restores the original environment" $ do
+      old <- getEnvironment
+      withModifiedEnvironment [("foo", "bar")] $ do
         return ()
       new <- getEnvironment
       new `shouldMatchList` old
